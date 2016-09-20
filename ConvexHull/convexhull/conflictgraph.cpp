ff232597a6e94b53970e26ec73a2f33970219a6c
iker92 <alex_pili@hotmail.it>
2016-09-20 09:28:28 +0200

    added conflict graph

---
+++ ConvexHull/convexhull/conflictgraph.cpp
@@ -0,0 +1,6 @@
+#include "conflictgraph.h"
+
+conflictgraph::conflictgraph()
+{
+
+}
---
+++ ConvexHull/convexhull/conflictgraph.h
@@ -0,0 +1,11 @@
+#ifndef CONFLICTGRAPH_H
+#define CONFLICTGRAPH_H
+
+
+class conflictgraph
+{
+public:
+    conflictgraph();
+};
+
+#endif // CONFLICTGRAPH_H
